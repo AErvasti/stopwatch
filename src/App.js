3fa5dc07ae65a7d3e36813a84a59ef047eba1902
Aleksi Ervasti <n0eral00@students.oamk.fi>
2021-02-02 13:01:32 +0200

    jotaki puuttuu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 import {useState} from 'react'
+import Navbar from './fragments/Navbar';
+import Header from './fragments/Header';
+import Footer from './fragments/Footer';
+import Home from './Home';
+import About from './About';
+import ContactUs from './ContactUs';
+import NotFound from './NotFound';
+import Route from 'react'
+import Switch from 'react'
+
 
 function App() {
   return (
-    <div>
-      <p className="text-primary">testi</p>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Header>
+      <div className="container">
+        <Switch>
+          <Route path="/" component={Home} exact></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/contactus" component={ContactUs}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
+      </div>
+      </Header>
+      <Footer></Footer>
+    </>
   );
 }
 
